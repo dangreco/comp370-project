0d474c0ad770befbc03d9b020f968e0821e596e4
Dan Greco <me@dangre.co>
2025-11-19 02:17:20 -0500

    Fix cd.yml

--- flake.nix
+++ flake.nix
@@ -21,99 +21,133 @@
           ...
         }:
         {
-          devShells.default =
-            let
-              __zed = pkgs.writeTextFile {
-                name = "__zed";
-                text = builtins.toJSON {
-                  lsp = {
-                    ty = {
-                      binary = {
-                        path = "${pkgs.ty}/bin/ty";
-                        arguments = [ "server" ];
+          devShells = {
+            default =
+              let
+                __zed = pkgs.writeTextFile {
+                  name = "__zed";
+                  text = builtins.toJSON {
+                    lsp = {
+                      ty = {
+                        binary = {
+                          path = "${pkgs.ty}/bin/ty";
+                          arguments = [ "server" ];
+                        };
                       };
                     };
-                  };
-                  languages = {
-                    Python = {
-                      language_servers = [
-                        "!pylsp"
-                        "!pyright"
-                        "!basedpyright"
-                        "ty"
-                      ];
-                      formatter = {
-                        external = {
-                          command = "${pkgs.ruff}/bin/ruff";
-                          arguments = [
-                            "format"
-                            "--stdin-filename"
-                            "{buffer_path}"
-                          ];
+                    languages = {
+                      Python = {
+                        language_servers = [
+                          "!pylsp"
+                          "!pyright"
+                          "!basedpyright"
+                          "ty"
+                        ];
+                        formatter = {
+                          external = {
+                            command = "${pkgs.ruff}/bin/ruff";
+                            arguments = [
+                              "format"
+                              "--stdin-filename"
+                              "{buffer_path}"
+                            ];
+                          };
                         };
                       };
                     };
                   };
+                  destination = "/settings.json";
                 };
-                destination = "/settings.json";
-              };
-              libraryPath =
-                with pkgs;
-                lib.makeLibraryPath [
+                libraryPath =
+                  with pkgs;
+                  lib.makeLibraryPath [
+                    stdenv.cc.cc
+                    openssl
+                    zlib
+                  ];
+              in
+              pkgs.mkShell {
+                buildInputs = with pkgs; [
                   stdenv.cc.cc
                   openssl
                   zlib
                 ];
-            in
-            pkgs.mkShell {
-              buildInputs = with pkgs; [
-                stdenv.cc.cc
-                openssl
-                zlib
-              ];
 
-              nativeBuildInputs = with pkgs; [
-                nil
-                nixd
-                nixfmt
+                nativeBuildInputs = with pkgs; [
+                  nil
+                  nixd
+                  nixfmt
 
-                git
-                act
-                go-task
-                husky
+                  git
+                  act
+                  go-task
+                  husky
 
-                # YAML
-                yamllint
-                yamlfmt
+                  # YAML
+                  yamllint
+                  yamlfmt
 
-                # TOML
-                tombi
+                  # TOML
+                  tombi
 
-                # Python
-                uv
-                ty
-                ruff
-                python313
+                  # Python
+                  uv
+                  ty
+                  ruff
+                  python313
 
-                # LaTeX
-                (texliveSmall.withPackages (
-                  p: with p; [
-                    latexmk
-                    chktex
-                  ]
-                ))
-                tex-fmt
-                zathura
-              ];
+                  # LaTeX
+                  (texliveSmall.withPackages (
+                    p: with p; [
+                      latexmk
+                      chktex
+                    ]
+                  ))
+                  tex-fmt
+                  zathura
+                ];
 
-              shellHook = ''
-                export "LD_LIBRARY_PATH=''$LD_LIBRARY_PATH:${libraryPath}"
-                rm -rf .zed || true
-                mkdir -p .zed || true
-                cp ${__zed}/settings.json .zed/settings.json || true
-                husky install > /dev/null 2>&1 || true
-              '';
-            };
+                shellHook = ''
+                  export "LD_LIBRARY_PATH=''$LD_LIBRARY_PATH:${libraryPath}"
+                  rm -rf .zed || true
+                  mkdir -p .zed || true
+                  cp ${__zed}/settings.json .zed/settings.json || true
+                  husky install > /dev/null 2>&1 || true
+                '';
+              };
+            build =
+              let
+                libraryPath =
+                  with pkgs;
+                  lib.makeLibraryPath [
+                    stdenv.cc.cc
+                    openssl
+                    zlib
+                  ];
+              in
+              pkgs.mkShell {
+                buildInputs = with pkgs; [
+                  stdenv.cc.cc
+                  openssl
+                  zlib
+                ];
+
+                nativeBuildInputs = with pkgs; [
+                  git
+                  go-task
+
+                  # Python
+                  uv
+                  ty
+                  ruff
+                  python313
+                ];
+
+                shellHook = ''
+                  export "LD_LIBRARY_PATH=''$LD_LIBRARY_PATH:${libraryPath}"
+                '';
+              };
+          };
         };
     };
 }
